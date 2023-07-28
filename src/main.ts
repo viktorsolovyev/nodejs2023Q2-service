75e6132a2b71fa60e69e707f408a684ea851104e
Viktor Solovyev <barn-mih@rambler.ru>
2023-07-28 18:44:49 +0300

    feat: add default port

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ import 'dotenv/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
